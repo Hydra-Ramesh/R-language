803a1dc663d52701809be2c2a1a53ff0157e01fd
Ramesh <dasramesh8343003905@gmail.com>
2024-07-14 05:03:03 +0530

    R language Bar Graph Section

--- 24. Scatter Plot/PlotCompare.R
+++ 24. Scatter Plot/PlotCompare.R
@@ -7,4 +7,4 @@ x2 <- c(2,2,8,1,15,8,12,9,7,3,11,4,7,14,12)#nolint
 y2 <- c(100,105,84,105,90,99,90,95,94,100,79,112,91,80,85)#nolint
 
 plot(x1, y1, main="Observation of Cars", xlab="Car age", ylab="Car speed", col="red", cex=2)#nolint
-points(x2, y2, col="blue", cex=2)#nolint
+points(x2, y2, col="blue", cex=4)#nolint
